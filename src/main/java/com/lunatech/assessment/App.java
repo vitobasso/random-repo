15b24b94d6281dbee1161311119376c76e3596e5
Victor Basso <vitobasso@gmail.com>
2015-12-01 11:14:08 -0300

    project structure

---
+++ src/main/java/com/lunatech/assessment/App.java
@@ -0,0 +1,11 @@
+
+package com.lunatech.assessment;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
